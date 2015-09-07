48fe6db12500e13013c160849ec35e4d08c6719c
Student <student@RU-109.local>
2015-09-07 13:44:11 -0600

    2nd

--- js2.js
+++ js2.js
@@ -1,3 +1,8 @@
 var input = prompt("Enter a word!");
 alert(input);
 alert("Your character count is " + input.length);
+alert("Your third character is " + input.charAt(2));
+alert("Your word in lowercase is " + input.toLowerCase());
+alert("Your word in Uppercase is " + input.toUpperCase());
+alert("Here's an example of " + input + " in a sentence.");
+alert("Here are the 2nd 3rd and 4th characters of " + input + ", " + input.substring(1, 4));
